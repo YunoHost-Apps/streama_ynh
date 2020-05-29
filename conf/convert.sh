c922ce54af60c47092b5064fedb41def8c7c4ce4
liberodark <liberodark@gmail.com>
2020-05-29 16:12:39 +0200

    Update convert.sh

--- conf/convert.sh
+++ conf/convert.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-for file in $(find ./ -iname "*.mkv" -o -iname "*.avi")
+for file in $(find /home/yunohost.app/streama/ -iname "*.mkv" -o -iname "*.avi")
 do
   ffmpeg -i "$file" -vcodec h264 -acodec aac -strict -2 "${file%.*}.mp4"
   rm "$file"
