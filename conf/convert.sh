df577a485cff270ae849357e8f1385fb42ac22be
liberodark <liberodark@gmail.com>
2020-05-29 16:07:43 +0200

    Update convert.sh

--- conf/convert.sh
+++ conf/convert.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
-for file in "$( find ./ -iname "*.mkv")"
-do
-  ffmpeg -i "$file" -vcodec h264 -acodec aac -strict -2 "${file%.*}.mp4"
-  rm "$file"
-done
-
-for file in "$( find ./ -iname "*.avi")"
+for file in $(find ./ -iname "*.mkv" -o -iname "*.avi")
 do
   ffmpeg -i "$file" -vcodec h264 -acodec aac -strict -2 "${file%.*}.mp4"
   rm "$file"
