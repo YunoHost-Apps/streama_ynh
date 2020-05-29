c70b4ecde6e0988445a882ecaf15c9e2c931ad3f
liberodark <liberodark@gmail.com>
2020-05-29 17:02:33 +0200

    Update convert.sh

--- conf/convert.sh
+++ conf/convert.sh
@@ -3,4 +3,4 @@ while IFS= read -r -d '' file
 do
   ffmpeg -nostdin -i "$file" -vcodec h264 -acodec aac -strict -2 "${file%.*}.mp4"
   rm -f "$file"
-done <   <(find /home/yunohost.app/streama/ -name '*.mkv' '*.avi' -print0)
+done <   <(find /home/yunohost.app/streama/ -name '*.mkv' -print0 -o -name '*.avi' -print0)
