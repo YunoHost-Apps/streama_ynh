c45a491fd952b926ca4a511cc695cadf3345f556
liberodark <liberodark@gmail.com>
2020-05-30 03:19:47 +0200

    Update convert.sh

--- conf/convert.sh
+++ conf/convert.sh
@@ -1,4 +1,21 @@
-#! /bin/bash
+#!/bin/bash
+#
+# About: Convert movies automatically
+# Author: liberodark
+# Thanks : 
+# License: GNU GPLv3
+
+version="0.1.0"
+
+#=================================================
+# RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
+#=================================================
+
+lock="/tmp/wp-sync.lock"
+
+exec 9>"${lock}"
+flock -n 9 || exit
+
 while IFS= read -r -d '' file
 do
   ffmpeg -nostdin -i "$file" -vcodec h264 -acodec aac -strict -2 "${file%.*}.mp4"
