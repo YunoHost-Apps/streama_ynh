65ee5f99c0b89fecbcd2908e4e7d0eda5216245c
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-14 12:48:09 +0200

    update to Bookworm

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="openjdk-11-jre-headless ffmpeg"
+pkg_dependencies="openjdk-17-jre-headless ffmpeg"
 
 #=================================================
 # PERSONAL HELPERS
