02a7c03c85d9747dbb238e161ae12c14807fc7cd
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-07-28 07:35:28 +0100

    Version 2 (#43)
    * update to Bookworm
    
    * v2
    
    * Auto-update README
    
    * v2
    
    * Auto-update README
    
    * Update manifest.toml
    
    * Update manifest.toml
    
    * Create DESCRIPTION.md
    
    * Auto-update README
    
    * fix
    
    * Update manifest.toml
    
    * Update manifest.toml
    
    * Create application.yaml
    
    * fix
    
    * Update restore
    
    * fix
    
    * Update application.yaml
    
    * Update application.yaml
    
    * FIX
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="openjdk-11-jre-headless ffmpeg"
+#REMOVEME? pkg_dependencies="openjdk-17-jre-headless ffmpeg"
 
 #=================================================
 # PERSONAL HELPERS
